45532b42e781ffed396d80b9da12dc2de91919d0
Dalia Ramirez <dalia.lurg@gmail.com>
2023-06-02 17:14:26 +0200

    Adding lepton veto cut

--- Taggers/plugins/BPbHTagProducer.cc
+++ Taggers/plugins/BPbHTagProducer.cc
@@ -48,21 +48,55 @@ namespace flashgg {
             EDGetTokenT<View<DiPhotonCandidate>> diPhotonToken_;
             EDGetTokenT<View<DiPhotonMVAResult>> mvaResultToken_;
 
+	    EDGetTokenT<View<flashgg::Muon>> muonToken_;
+	    EDGetTokenT<View<Electron>> electronToken_;
+	    EDGetTokenT<View<reco::Vertex> > vertexToken_;
+
             double leadPhoOverMassThreshold_;
             double subleadPhoOverMassThreshold_;
             double PhoMVAThreshold_;
+
+            //leptons
+            
+            double MuonEtaCut_;
+            double MuonPtCut_;
+            double MuonIsoCut_;
+            double MuonPhotonDrCut_;
+            double ElePtCut_;
+            std::vector<double> EleEtaCuts_;
+            double ElePhotonDrCut_;
+            double ElePhotonZMassCut_;
+            double DeltaRTrkEle_;
+            bool   debug_;
 
     }; // closing 'class BPbHTagProducer'
 
     // ----------
 
     BPbHTagProducer::BPbHTagProducer(const ParameterSet &iConfig) :
-        diPhotonToken_(consumes<View<flashgg::DiPhotonCandidate>>(iConfig.getParameter<InputTag> ("DiPhotonTag"))),
-        mvaResultToken_(consumes<View<flashgg::DiPhotonMVAResult>>(iConfig.getParameter<InputTag> ("MVAResultTag"))) {
+        diPhotonToken_(consumes<View<flashgg::DiPhotonCandidate>>(iConfig.getParameter<InputTag> ("DiPhotonTag")))
+       ,mvaResultToken_(consumes<View<flashgg::DiPhotonMVAResult>>(iConfig.getParameter<InputTag> ("MVAResultTag"))) 
+       ,muonToken_(consumes<View<flashgg::Muon> >(iConfig.getParameter<InputTag>("MuonTag")))
+       ,electronToken_(consumes<View<flashgg::Electron> >(iConfig.getParameter<InputTag>("ElectronTag")))
+       ,vertexToken_( consumes<View<reco::Vertex> >( iConfig.getParameter<InputTag> ( "VertexTag" ) ) ) 
+{
 
         leadPhoOverMassThreshold_ = iConfig.getParameter<double>("leadPhoOverMassThreshold");
         subleadPhoOverMassThreshold_ = iConfig.getParameter<double>("subleadPhoOverMassThreshold");
         PhoMVAThreshold_ = iConfig.getParameter<double>("PhoMVAThreshold");
+
+	MuonEtaCut_ = iConfig.getParameter<double>( "MuonEtaCut");
+        MuonPtCut_ = iConfig.getParameter<double>( "MuonPtCut");
+        MuonIsoCut_ = iConfig.getParameter<double>( "MuonIsoCut");
+        MuonPhotonDrCut_ = iConfig.getParameter<double>( "MuonPhotonDrCut");
+ 
+        EleEtaCuts_ = iConfig.getParameter<std::vector<double>>( "EleEtaCuts");
+        ElePtCut_ = iConfig.getParameter<double>( "ElePtCut");
+        ElePhotonDrCut_ = iConfig.getParameter<double>( "ElePhotonDrCut");
+        ElePhotonZMassCut_ = iConfig.getParameter<double>( "ElePhotonZMassCut");
+        DeltaRTrkEle_ = iConfig.getParameter<double>( "DeltaRTrkEle");
+
+        debug_ = iConfig.getParameter<bool>( "debug" );
 
         produces<vector<BPbHTag>>();
 
@@ -79,6 +113,15 @@ namespace flashgg {
         Handle<View<flashgg::DiPhotonMVAResult> > mvaResults;
         evt.getByToken(mvaResultToken_, mvaResults);
 
+        Handle<View<flashgg::Muon> > theMuons;
+	evt.getByToken(muonToken_, theMuons );
+
+	Handle<View<flashgg::Electron> > theElectrons;
+        evt.getByToken(electronToken_, theElectrons );
+
+	Handle<View<reco::Vertex> > vertices;
+	evt.getByToken( vertexToken_, vertices );
+
         // Declaring and initializing variables
         double idmva1 = 0.0;
         double idmva2 = 0.0;
@@ -86,6 +129,7 @@ namespace flashgg {
         std::unique_ptr<vector<BPbHTag> > bpbhtags( new vector<BPbHTag> );
 
         std::cout << "=======> New Event with " << diPhotons->size() << " diphoton candidates" << std::endl; 
+        //std::cout << "leading Pho over Mass Threshold " << leadPhoOverMassThreshold_  << std::endl;
 
         // Loop over diphoton candidates
         for (unsigned int diphoIndex=0; diphoIndex < diPhotons->size(); diphoIndex++) {
@@ -95,15 +139,34 @@ namespace flashgg {
 
             idmva1 = dipho->leadingPhoton()->phoIdMvaDWrtVtx(dipho->vtx());
             idmva2 = dipho->subLeadingPhoton()->phoIdMvaDWrtVtx(dipho->vtx());
+            
+            // std::cout << "diphoIndex " << diphoIndex << " leading pT: " << dipho->leadingPhoton()->pt() << std::endl;
+            // std::cout << "leading g Pt " << dipho->leadingPhoton()->pt() << std::endl;
+            // std::cout << "subLeading g Pt " << dipho->subLeadingPhoton()->pt() << std::endl;
 
-            std::cout << "diphoIndex " << diphoIndex << " leading pT: " << dipho->leadingPhoton()->pt() << std::endl;
-
-            // Diphoton selection:
+            // Cut 1: diphoton selection:
             // - pT cuts on leading and subleading photons
             // - cut on photon ID MVA
             if (dipho->leadingPhoton()->pt() < (dipho->mass())*leadPhoOverMassThreshold_) { continue; }
+            // std::cout << "dipho_mass * leading Pho over Mass Threshold" << (dipho->mass())*leadPhoOverMassThreshold_ << std::endl; 
             if (dipho->subLeadingPhoton()->pt() < (dipho->mass())*subleadPhoOverMassThreshold_) { continue; }
             if (idmva1 < PhoMVAThreshold_ || idmva2 < PhoMVAThreshold_) { continue; }
+
+            // Cut 2: lepton veto
+            std::vector<edm::Ptr<flashgg::Muon>> Muons;
+            std::vector<edm::Ptr<flashgg::Electron>> Electrons;
+
+	    if (theMuons->size()>0)
+	    	Muons = selectMuons(theMuons->ptrs(), dipho, vertices->ptrs(), MuonPtCut_, MuonEtaCut_, MuonIsoCut_, MuonPhotonDrCut_, debug_);
+
+	    if (theElectrons->size()>0)
+                Electrons = selectElectrons(theElectrons->ptrs(), dipho, ElePtCut_, EleEtaCuts_, ElePhotonDrCut_, ElePhotonZMassCut_, DeltaRTrkEle_, debug_);
+
+	    std::cout <<" Number of leptons " << (Muons.size() + Electrons.size()) << std::endl;
+
+	    if ( (Muons.size() + Electrons.size()) != 0 ) continue;
+
+	
 
             std::cout << "Passed cuts!" << std::endl;
 
--- Taggers/python/flashggBPbH_cff.py
+++ Taggers/python/flashggBPbH_cff.py
@@ -5,5 +5,19 @@ flashggBPbH = cms.EDProducer("FlashggBPbHTagProducer",
                              MVAResultTag = cms.InputTag('flashggDiPhotonMVA'),
                              leadPhoOverMassThreshold = cms.double(0.33),
                              subleadPhoOverMassThreshold = cms.double(0.25),
-                             PhoMVAThreshold = cms.double(-0.7)
-                             ) 
+                             PhoMVAThreshold = cms.double(-0.7),
+			     ElectronTag=cms.InputTag('flashggSelectedElectrons'),
+			     MuonTag=cms.InputTag('flashggSelectedMuons'),
+			     VertexTag=cms.InputTag('offlineSlimmedPrimaryVertices'),                             
+			     MuonEtaCut = cms.double(2.4),
+                             MuonPtCut = cms.double(5),
+                             MuonIsoCut = cms.double(0.25),
+                             MuonPhotonDrCut = cms.double(0.),
+                             EleEtaCuts = cms.vdouble(1.4442,1.566,2.5),
+                             ElePtCut = cms.double(10),
+                             ElePhotonDrCut = cms.double(0.),
+                             ElePhotonZMassCut = cms.double(5),
+                             DeltaRTrkEle = cms.double(0.),
+                             debug = cms.bool(False)
+
+) 
